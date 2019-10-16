290adfcfcd8d40a0b12c462a33a524ac28dceac7
Massimo Oliviero <m.oliviero@codermine.com>
2019-10-16 10:08:44 +0200

    Added vertical and horizontal priority params in AutolayoutSize to better control size calculation.

--- PulsarKit/PulsarKit/Sources/Sizes/AutolayoutSize.swift
+++ PulsarKit/PulsarKit/Sources/Sizes/AutolayoutSize.swift
@@ -8,7 +8,15 @@
 
 import UIKit
 
-public struct AutolayoutSize: Sizeable {    
+public struct AutolayoutSize: Sizeable {
+    public let verticalFittingPriority: UILayoutPriority
+    public let horizontalFittingPriority: UILayoutPriority
+    
+    public init(horizontalFittingPriority: UILayoutPriority = .defaultHigh, verticalFittingPriority: UILayoutPriority = .fittingSizeLevel) {
+        self.verticalFittingPriority = verticalFittingPriority
+        self.horizontalFittingPriority = horizontalFittingPriority
+    }
+    
     public func size<View: UIView>(for view: View, descriptor: Descriptor, model: AnyHashable, in container: UIScrollView) -> CGSize {
         // starting bounds
         var bounds = container.bounds
@@ -28,8 +36,8 @@ public struct AutolayoutSize: Sizeable {
         
         // run autolayout
         let viewSize = view.systemLayoutSizeFitting(bounds.size,
-                                                    withHorizontalFittingPriority: .defaultHigh,
-                                                    verticalFittingPriority: .fittingSizeLevel)
+                                                    withHorizontalFittingPriority: horizontalFittingPriority,
+                                                    verticalFittingPriority: verticalFittingPriority)
         
         // cache
         return viewSize
